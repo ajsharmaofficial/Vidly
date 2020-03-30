60a509551c1833ca9013c496b06337d289c9434e
Ajay <ajaykumarsharma578@gmail.com>
2020-03-30 00:58:59 -0400

    Added EF6 and created DAL

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,36 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.DAL;
 using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: Customers
         [Route("Customers")]
         public ActionResult Index()
         {
-            var customers = new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Ajay Sharma"},
-                new Customer { Id=2, Name = "Aakash"}
-            };
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
             return View(customers);
         }
 
         [Route("Customers/details/{id}")]
         public ActionResult Details(int id)
         {
-            var customers = new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Ajay Sharma"},
-                new Customer { Id=2, Name = "Aakash"}
-            };
-            var customer = customers.Where(c=>c.Id == id).SingleOrDefault();
+            
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             return View(customer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
         }
     }
 }
